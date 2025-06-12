71f936389b71b71bd619de9cb6d8532fbb823a87
BChYu <a1848916416@163.com>
2025-06-12 17:45:10 +0800

    第三天修改

--- api/github_api.py
+++ api/github_api.py
@@ -1,3 +1,4 @@
+import pytest
 import requests
 
 #编写API封装类
@@ -7,11 +8,22 @@ class GitHubAPI:
         self.headers = {
             'Accept': 'application/vnd.github.v3+json'#设置 HTTP 请求头（header） accept指定客户端期望接收的数据格式
         }
+        self.session = requests.Session()
+        self.session.headers.update(self.headers)
+    # def get_user(self, username):
+    #     '''获取用户信息'''
+    #     url = f'{self.base_url}/users/{username}'
+    #     response = requests.get(url, headers = self.headers)
+    #     return response
     def get_user(self, username):
         '''获取用户信息'''
-        url = f'{self.base_url}/users/{username}'
-        response = requests.get(url, headers = self.headers)
-        return response
+        try:
+            url = f'{self.base_url}/users/{username}'
+            response = self.session.get(url, timeout=5)
+            response.raise_for_status()
+            return response
+        except requests.exceptions.RequestException as e:
+            pytest.fail(f'API请求失败：{str(e)}')
     def get_repo(self, owner, repo_name):
         '''获取仓库信息'''
         url = f"{self.base_url}/repos/{owner}/{repo_name}"
